3cc4ebdac5cb0af53825d252d5ce413341c5eeec
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-17 17:55:06 -0500

    Adding code from section "4.4.7. (1:16) Exercise: Set MyCanvasView as the Content View"

--- app/src/main/java/com/example/android/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/android/minipaint/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.android.minipaint
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+	@Suppress("DEPRECATION")
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+
+		val myCanvasView = MyCanvasView(this)
+		myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+		myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+		setContentView(myCanvasView)
 	}
 }
