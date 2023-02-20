65363d1eeab6e909d7ca987e4f0dbf57f3377148
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-02-20 18:37:32 -0500

    Adding code from section "4.4.10. (2:28) Exercise: Set up Paint for Drawing"

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -3,8 +3,11 @@ package com.example.android.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
+
+private const val STROKE_WIDTH = 12f // has to be float
 
 class MyCanvasView(context: Context) : View(context) {
 
@@ -14,6 +17,22 @@ class MyCanvasView(context: Context) : View(context) {
 	private val backgroundColor = ResourcesCompat.getColor(
 		resources, R.color.colorBackground, null
 	)
+
+	private val drawColor = ResourcesCompat.getColor(
+		resources, R.color.colorPaint, null
+	)
+
+	private val paint = Paint().apply {
+		color = drawColor
+		// Smooths out edges of what is drawn without affecting shape.
+		isAntiAlias = true
+		// Dithering affects how colors with higher-precision than the device are down-sampled.
+		isDither = true
+		style = Paint.Style.STROKE // default: FILL
+		strokeJoin = Paint.Join.ROUND // default: MITER
+		strokeCap = Paint.Cap.ROUND // default: BUTT
+		strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+	}
 
 	override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
 		super.onSizeChanged(width, height, oldWidth, oldHeight)
